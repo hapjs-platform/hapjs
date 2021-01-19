0b0cc7768d83cee7d59fc140a28a2ad13c52b3e7
chaihua1 <chaihua1@xiaomi.com>
2021-01-19 15:02:18 +0800

    快应用文字大小适配 fix #47
    Change-Id: I5b18b25085691f298d715590e774818705be9811

--- core/runtime/android/runtime/src/main/java/org/hapjs/runtime/Runtime.java
+++ core/runtime/android/runtime/src/main/java/org/hapjs/runtime/Runtime.java
@@ -24,6 +24,8 @@ import org.hapjs.common.net.UserAgentHelper;
 import org.hapjs.common.utils.FrescoUtils;
 import org.hapjs.common.utils.ProcessUtils;
 import org.hapjs.common.utils.SoLoaderHelper;
+import org.hapjs.component.constants.DefaultFontSizeProvider;
+import org.hapjs.component.constants.FontSizeProvider;
 import org.hapjs.pm.DefaultNativePackageProviderImpl;
 import org.hapjs.pm.NativePackageProvider;
 import org.hapjs.render.jsruntime.Profiler;
@@ -156,6 +158,7 @@ public class Runtime {
                 .addProvider(InstallInterceptProvider.NAME,
                         new DefaultInstallInterceptProviderImpl());
         pm.addProvider(HybridDialogProvider.NAME, new DefaultHybridDialogProviderImpl());
+        pm.addProvider(FontSizeProvider.NAME, new DefaultFontSizeProvider());
         if (!mLazyLoad) {
             load();
         }
