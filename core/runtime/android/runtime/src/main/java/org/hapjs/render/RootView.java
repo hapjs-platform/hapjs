531739ac35c8f43531936654a70a95b338f98d22
limingxiao <mingxiao.li@vivo.com>
2021-04-20 16:53:05 +0800

    [ISSUE-1819]:花样短视频在Android5的机型上，灭屏后可以继续播放
    Change-Id: I09894a005b5c9b84cc726b9a522470ac57a3f989

--- core/runtime/android/runtime/src/main/java/org/hapjs/render/RootView.java
+++ core/runtime/android/runtime/src/main/java/org/hapjs/render/RootView.java
@@ -13,6 +13,7 @@ import android.graphics.Color;
 import android.graphics.Rect;
 import android.hardware.display.DisplayManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
@@ -611,6 +612,10 @@ public class RootView extends FrameLayout
                     };
         }
         mDisplayManager.registerDisplayListener(mDisplayListener, null);
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            onVisibilityChanged(this, getVisibility());
+        }
     }
 
     @Override
