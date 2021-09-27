4f4d0c49227426ca3b7cd7cd0f5c632f82cb207d
gen.tian <gen.tian@vivo.com>
2021-09-27 16:55:13 +0800

    [ISSUE-1847]引擎内部feature接口调用抽离并放到jsthread线程

---
+++ core/runtime/android/runtime/src/main/java/org/hapjs/common/utils/FeatureInnerBridge.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2022, hapjs.org. All rights reserved.
+ */
+
+package org.hapjs.common.utils;
+
+import android.util.Log;
+
+import org.hapjs.bridge.Callback;
+import org.hapjs.bridge.ExtensionManager;
+import org.hapjs.bridge.HybridManager;
+import org.hapjs.bridge.Response;
+import org.hapjs.render.jsruntime.JsThread;
+
+public class FeatureInnerBridge {
+    public static final String TAG = "FeatureInnerBridge";
+    public static final String H5_JS_CALLBACK = "-2";
+
+    public static void invokeWithCallback(ExtensionManager extensionManager, String name, String action, Object rawParams,
+                                          String jsCallback, int instanceId, Callback asyncResultCallback) {
+        final ExtensionManager tmpExtensionManager = extensionManager;
+        if (null == tmpExtensionManager) {
+            Response response = new Response(Response.CODE_GENERIC_ERROR,
+                    "invokeWithCallback tmpExtensionManager is null ,Refuse to use this interfaces in background: " + name);
+            if (asyncResultCallback != null) {
+                asyncResultCallback.callback(response);
+            }
+            return;
+        }
+        JsThread jsThread = tmpExtensionManager.getJsThread();
+        if (null != jsThread) {
+            jsThread.postInJsThread(new Runnable() {
+                @Override
+                public void run() {
+                    HybridManager tmpHybridManager = tmpExtensionManager.getHybridManager();
+                    if (null == tmpHybridManager) {
+                        Log.e(TAG, "invokeWithCallback error tmpHybridManager null.");
+                        Response response = new Response(Response.CODE_GENERIC_ERROR,
+                                "invokeWithCallback tmpHybridManager is null ,Refuse to use this interfaces in background: " + name);
+                        if (asyncResultCallback != null) {
+                            asyncResultCallback.callback(response);
+                        }
+                        return;
+                    }
+                    tmpExtensionManager.onInvoke(name, action, rawParams, jsCallback, instanceId, asyncResultCallback);
+                }
+            });
+        } else {
+            Log.e(TAG, "invokeWithCallback jsThread null.");
+            Response response = new Response(Response.CODE_GENERIC_ERROR,
+                    "invokeWithCallback jsThread is null ,Refuse to use this interfaces in background: " + name);
+            if (asyncResultCallback != null) {
+                asyncResultCallback.callback(response);
+            }
+        }
+    }
+}
