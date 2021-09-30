2e7259109054fb15b2e68a7ae6b874d561a1208e
cmc <mingcan.chen@vivo.com>
2021-09-30 15:43:32 +0800

    [#505]全屏状态栏问题

--- core/runtime/android/runtime/src/main/java/org/hapjs/render/vdom/DocComponent.java
+++ core/runtime/android/runtime/src/main/java/org/hapjs/render/vdom/DocComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022, the hapjs-platform Project Contributors
+ * Copyright (c) 2021-present, the hapjs-platform Project Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -71,6 +71,7 @@ public class DocComponent extends Container {
                         FrameLayout.LayoutParams.MATCH_PARENT);
         ViewUtils.fitParentLayoutParams(lp, rootView);
         mDecorLayout.setLayoutParams(lp);
+        mCallback.addActivityStateListener(this);
     }
 
     public boolean hasWebComponent() {
