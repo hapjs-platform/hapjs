dddf9f64a7c2980f394b372b783771c4244495a8
RanLC <v-ranlingcai@oppo.com>
2022-09-23 09:22:40 +0800

    [#348]增加lottie组件

--- core/runtime/android/widgets/src/main/java/org/hapjs/widgets/view/lottie/FlexLottie.java
+++ core/runtime/android/widgets/src/main/java/org/hapjs/widgets/view/lottie/FlexLottie.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2021-2022, hapjs.org. All rights reserved.
+ * Copyright (c) 2021-2022, the hapjs-platform Project Contributors
+ * SPDX-License-Identifier: Apache-2.0
  */
 package org.hapjs.widgets.view.lottie;
 
