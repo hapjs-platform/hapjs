a90c6032d7e988119c6bed37a798879e4f03c82c
panjianbo <panjianbo@oppo.com>
2023-11-17 10:55:17 +0800

    [#610]锁屏keyguard新增拉起键盘接口

--- core/runtime/android/features/src/main/java/org/hapjs/features/Keyguard.java
+++ core/runtime/android/features/src/main/java/org/hapjs/features/Keyguard.java
@@ -5,8 +5,10 @@
 
 package org.hapjs.features;
 
+import android.app.Activity;
 import android.app.KeyguardManager;
 import android.content.Context;
+
 import org.hapjs.bridge.FeatureExtension;
 import org.hapjs.bridge.Request;
 import org.hapjs.bridge.Response;
@@ -19,12 +21,13 @@ import org.json.JSONObject;
         name = Keyguard.FEATURE_NAME,
         actions = {
                 @ActionAnnotation(
-                        name = Keyguard.ACTION_GET_KEYGUARD_LOCKED_STATUS,
-                        mode = FeatureExtension.Mode.ASYNC)
+                        name = Keyguard.ACTION_GET_KEYGUARD_LOCKED_STATUS, mode = FeatureExtension.Mode.ASYNC),
+                @ActionAnnotation(name = Keyguard.ACTION_REQUEST_DISMISS_KEYGUARD, mode = FeatureExtension.Mode.ASYNC)
         })
 public class Keyguard extends FeatureExtension {
     protected static final String FEATURE_NAME = "system.keyguard";
     protected static final String ACTION_GET_KEYGUARD_LOCKED_STATUS = "getKeyguardLockedStatus";
+    protected static final String ACTION_REQUEST_DISMISS_KEYGUARD = "requestDismissKeyguard";
     protected static final String RESULT_KEY_IS_KEYGUARD_LOCKED = "isKeyguardLocked";
 
     @Override
@@ -37,6 +40,8 @@ public class Keyguard extends FeatureExtension {
         String action = request.getAction();
         if (ACTION_GET_KEYGUARD_LOCKED_STATUS.equals(action)) {
             getKeyguardLockedStatus(request);
+        } else if (ACTION_REQUEST_DISMISS_KEYGUARD.equals(action)) {
+            requestDismissKeyguard(request);
         }
         return null;
     }
@@ -56,5 +61,42 @@ public class Keyguard extends FeatureExtension {
         } else {
             request.getCallback().callback(Response.ERROR);
         }
+    }
+
+    private void requestDismissKeyguard(Request request) {
+        Activity activity = request.getNativeInterface().getActivity();
+        if (activity == null || activity.isFinishing() || activity.isDestroyed()) {
+            request.getCallback().callback(Response.ERROR);
+            return;
+        }
+        if (activity.getWindow() == null) {
+            request.getCallback().callback(Response.ERROR);
+            return;
+        }
+
+        KeyguardManager keyguardManager = (KeyguardManager) activity.getSystemService(Context.KEYGUARD_SERVICE);
+        if (keyguardManager == null) {
+            request.getCallback().callback(Response.ERROR);
+            return;
+        }
+        keyguardManager.requestDismissKeyguard(activity, new KeyguardManager.KeyguardDismissCallback() {
+            @Override
+            public void onDismissError() {
+                super.onDismissError();
+                request.getCallback().callback(Response.ERROR);
+            }
+
+            @Override
+            public void onDismissSucceeded() {
+                super.onDismissSucceeded();
+                request.getCallback().callback(Response.SUCCESS);
+            }
+
+            @Override
+            public void onDismissCancelled() {
+                super.onDismissCancelled();
+                request.getCallback().callback(Response.CANCEL);
+            }
+        });
     }
 }
